f73fecf7da820c03889e098334907cbcda61052e
sajjadhm14 <sajjadhamidi499@gmail.com>
2025-08-29 19:38:03 +0330

    make contactus factory

---
+++ database/factories/ContactUsFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ContactUs>
+ */
+class ContactUsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->word(),
+            'description' => $this->faker->paragraph(),
+            'instagram'=>$this->faker->text(),
+        ];
+    }
+}
